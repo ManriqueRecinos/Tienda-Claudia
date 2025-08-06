38971798d082eb309b9421d1fbd15930d0b27437
Manrique Recinos <manrique.recinos23@gmail.com>
2025-08-06 13:40:56 -0600

    Integracion de tailwindcss

--- app/views/inc/headers.php
+++ app/views/inc/headers.php
@@ -2,3 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Titulo de la aplicacion -->
 <title><?php echo APP_NAME; ?></title>
+<!-- Tailwind -->
+<link rel="stylesheet" href="./public/assets/css/styles.css">
