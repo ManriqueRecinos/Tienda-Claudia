bccef5bb2c59c9a4d75331b0cab612f0d182041f
Manrique Recinos <manrique.recinos23@gmail.com>
2025-08-06 10:44:12 -0600

    Configuracion de la estructura inicial del proyecto con configuración de base de datos y plantillas HTML básicas

---
+++ app/views/inc/headers.php
@@ -0,0 +1,4 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Titulo de la aplicacion -->
+<title><?php echo APP_NAME; ?></title>
--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <!-- Aqui se ejecturara toda la aplicacion -->
 <?php
-require_once 'Config/app.php';
-require_once 'Config/Conexion.php';
-require_once 'Config/Core.php';
-require_once 'Config/server.php';
+require_once './Config/app.php';
+require_once './Config/Conexion.php';
+require_once './Config/Core.php';
+require_once './Config/server.php';
 require_once './autoload.php';
 
+// Ruta de la aplicacion
+if(isset($_GET['views'])){
+    $url = explode("/", $_GET['views']);
+}else{
+    $url = ["login"];
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!-- Headers -->
+    <?php include './app/views/inc/headers.php'; ?>
+</head>
+<body>
+    <!-- Footers -->
+    <?php include './app/views/inc/footers.php'; ?>
+</body>
+</html>
