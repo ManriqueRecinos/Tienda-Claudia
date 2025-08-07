77bbde2449758921aa49237f02de79f067b0597f
Manrique Recinos <manrique.recinos23@gmail.com>
2025-08-06 20:30:56 -0600

    fix shadow sm

--- app/views/contents/Register/register.php
+++ app/views/contents/Register/register.php
@@ -11,7 +11,7 @@
         </div>
         <form class="mt-8 space-y-6" action="#" method="POST">
             <input type="hidden" name="remember" value="true">
-            <div class="rounded-md shadow-sm -space-y-px">
+            <div class="rounded-md -space-y-px">
                 <div class="mb-4">
                     <label for="nombre" class="block text-sm font-medium text-gray-700 mb-1">Nombre completo</label>
                     <input id="nombre" name="nombre" type="text" required class="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Nombre completo">
