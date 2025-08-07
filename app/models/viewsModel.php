a42eb0c9be0336539ba0afe1679aaf13aff3d47e
Manrique Recinos <manrique.recinos23@gmail.com>
2025-08-06 20:34:21 -0600

    Caso especial para login y index

--- app/models/viewsModel.php
+++ app/models/viewsModel.php
@@ -4,28 +4,47 @@ namespace app\models;
 class viewsModel{
     protected function obtenerVistasModelo($vista){
         // Lista blanca de vistas: Solo se permitiran ciertas vistas
-        $listaBlanca = ["login", "register", "dashboard", "test"];
+        $listaBlanca = ["dashboard", "test"];
         
         if(in_array($vista, $listaBlanca)){
             // Verificar si existe en la carpeta temática correspondiente (primera letra mayúscula)
             $vistaMayus = ucfirst($vista); // Convertir primera letra a mayúscula
             if(is_file("./app/views/contents/{$vistaMayus}/{$vista}.php")){
-                return "./app/views/contents/{$vistaMayus}/{$vista}.php";
+                $contenido = "./app/views/contents/{$vistaMayus}/{$vista}.php";
             }
             // Verificar si existe en la carpeta Dashboard
             else if(is_file("./app/views/contents/Dashboard/" . $vista . ".php")){
-                return "./app/views/contents/Dashboard/" . $vista . ".php";
+                $contenido = "./app/views/contents/Dashboard/" . $vista . ".php";
             } 
             // Verificar si existe en la carpeta principal
             else if(is_file("./app/views/contents/" . $vista . ".php")){
-                return "./app/views/contents/" . $vista . ".php";
+                $contenido = "./app/views/contents/" . $vista . ".php";
             }
             else{
-                return "./app/views/contents/Error/404.php";
+                $contenido = "./app/views/contents/Error/404.php";
             }
-        } else {
-            return "./app/views/contents/Error/404.php";
+        } 
+        // Caso especial para login o index
+        elseif($vista=="login" || $vista=="index"){
+            if(is_file("./app/views/contents/Login/login.php")){
+                $contenido = "./app/views/contents/Login/login.php";
+            } else {
+                $contenido = "./app/views/contents/login.php";
+            }
         }
+        // Caso especial para register
+        elseif($vista=="register"){
+            if(is_file("./app/views/contents/Register/register.php")){
+                $contenido = "./app/views/contents/Register/register.php";
+            } else {
+                $contenido = "./app/views/contents/register.php";
+            }
+        }
+        else{
+            $contenido = "./app/views/contents/Error/404.php";
+        }
+        
+        return $contenido;
     }
 }
 ?>
